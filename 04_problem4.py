8160f41eb2c38928be578758ae0095175c805a60
Dia Pal <diap76046@gmail.com>
2026-02-28 04:21:13 +0530

    Add files via upload

---
+++ 04_problem4.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0   or n == 1:
+        result 1
+    else:
+        result= 1 
+    for i in range (2,n+1):
+        result  *= i
+    
+    
+n = int(input("Enter a number"))
+print(factorial(n))
+
+    
+    
